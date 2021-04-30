dd75a245efe52f3785d01fa09438180255755a86
ramya <ramya.rolly@gmail.com>
2021-04-30 16:39:12 -0400

    google clone with next

---
+++ keys.js
@@ -0,0 +1,5 @@
+//API keyhttps://developers.google.com/custom-search/v1/using_rest
+export const API_KEY ="AIzaSyACQ6qHxQazviKY7Ov_Lu0JxRFfiCeLX88";
+
+//Context keyhttps://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "2215115828884ed38";
